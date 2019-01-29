5760a2af2d040139d569cd5c66a7582fe9964e82
felixjtdb <felixjtdb@gmail.com>
2019-01-29 16:50:07 +0000

    Created TreeViewer component that renders Axe and Water components

---
+++ components/TreeViewer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+import Tree from './Tree.js'
+import Axe from './Axe.js'
+import Water from './Water.js'
+
+export default class TreeViewer extends Component {
+
+  render() {
+    return (
+      <View>
+        <Tree name={this.props.name} imageURL={this.props.imageURL} description={this.props.description} />
+        <View stlyes={styles.axe_water_container}>
+          <Axe />
+          <Water />
+        </View>
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  axe_water_container: {
+    flexDirection: "row"
+  }
+});
