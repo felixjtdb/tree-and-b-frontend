ec14c31fd0ade826171319d99b0180babba38589
rbbri <robin.s.johnson@gmail.com>
2019-01-29 12:15:38 +0000

    Initial auth setup
    Adds JWT-decode as a dependency. Auth needs to be extracted from app.js 
    and environment variables protected

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthSession } from 'expo';
+import jwtDecoder from 'jwt-decode';
 import Tree from './components/Tree';
 import Forest from './components/Forest';
 
+// Need to be extracted into environment variables
+const auth0ClientId = 'aZubr17XYtkDNEpHo6aT8yUkhCHJaBbp'
+const auth0Domain = 'https://tree-and-b.eu.auth0.com'
+
+// Creates a query string
+function toQueryString(params) {
+  return '?' + Object.entries(params)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
+}
+
+
 export default class App extends Component {
+ state = {
+   user_id: undefined,
+ };
+
+ _loginWithAuth0 = async () => {
+  const redirectUrl = AuthSession.getRedirectUrl();
+  console.log(`Redirect URL (add this to Auth0): ${redirectUrl}`);
+  const result = await AuthSession.startAsync({
+    authUrl: `${auth0Domain}/authorize` + toQueryString({
+      client_id: auth0ClientId,
+      response_type: 'token',
+      scope: 'openid name',
+      redirect_uri: redirectUrl,
+    }),
+  });
+
+  console.log(result);
+  if (result.type === 'success') {
+    this.handleParams(result.params);
+  }
+  }
+
+handleParams = (responseObj) => {
+  if (responseObj.error) {
+    Alert.alert('Error', responseObj.error_description
+      || 'something went wrong while logging in');
+    return;
+  }
+  const encodedToken = responseObj.id_token;
+  const decodedToken = jwtDecoder(encodedToken);
+  const username = decodedToken.name;
+  this.setState({ username });
+  }
+
+
   render() {
     return (
       <View>
-        <Forest />
+      {this.state.username !== undefined ?
+        <Forest /> :
+        <Text>Auth0 login</Text>
+        <Button
+          title="Login with Auth0"
+          onPress={this._loginWithAuth0}
+          />
       </View>
     );
   }
