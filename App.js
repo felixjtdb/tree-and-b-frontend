1ad20ecb62afe0d241191d353be4835cb58599e1
felixjtdb <felixjtdb@gmail.com>
2019-02-05 17:15:19 +0000

    Added fetch function onMount of App

--- App.js
+++ App.js
@@ -4,11 +4,34 @@ import Tree from './components/Tree';
 import Forest from './components/Forest';
 import TreeViewer from './components/TreeViewer';
 
+const ALL_TREES_URL = 'localhost:3000/trees'
+
 export default class App extends Component {
+  state = {
+    data: ''
+  }
+
+  componentDidMount = () => {
+    fetch(ALL_TREES_URL, {
+      method: 'GET'
+    })
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      this.setState({
+        data: responseJSON.body
+      })
+    })
+    .catch(error => {
+      this.setState({
+        data: 'ERROR WHEN FETCHING TREES... THE FOREST IS ON FIRE!!'
+      })
+    })
+  }
+
   render() {
     return (
       <View style={styles.defaultSpacing}>
-        <TreeViewer imageURL={'http://www.irishtreesociety.com/wp-content/uploads/irish-tree-society-feature.jpg'}/>
+        <Text> {this.state.data} </Text>
       </View>
     );
   }
@@ -23,3 +46,5 @@ const styles = StyleSheet.create({
     top: 50
   }
 });
+
+// <TreeViewer imageURL={'http://www.irishtreesociety.com/wp-content/uploads/irish-tree-society-feature.jpg'}/>
