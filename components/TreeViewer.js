620f7ae1e463e3a443ca822b10497ddd9e760727
felixjtdb <felixjtdb@gmail.com>
2019-02-08 17:04:13 +0000

    Added the ability to swipe trees in Treeviewer Component

--- components/TreeViewer.js
+++ components/TreeViewer.js
@@ -4,32 +4,48 @@ import Tree from './Tree.js'
 import Axe from './Axe.js'
 import Water from './Water.js'
 
+import SwipeCards from 'react-native-swipe-cards';
+
 export default class TreeViewer extends Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
-      <View style={styles.treeviewer}>
-        <View>
-          <Tree name={this.props.name} imageURL={this.props.imageURL} description={this.props.description} />
-        </View>
-        <View stlye={styles.axe_water_container}>
-          <Axe />
-          <Water />
-        </View>
-      </View>
+      <SwipeCards style={styles.swipe_cards}
+        cards={this.props.trees}
+        renderCard={(treeData) => <Tree {...treeData}/> }
+
+        handleYup={this.saveToForest}
+        handleNope={this.burnTree}
+      />
     );
   }
 
+  saveToForest() {
+    console.log('YEP')
+  }
+
+  burnTree() {
+    console.log('NOPE')
+  }
 }
 
 const styles = StyleSheet.create({
-  axe_water_container: {
-    flex: 2,
-    flexDirection: 'column',
-    alignContent: 'center',
-  },
-  treeviewer: {
-    flexDirection: 'row',
-    alignContent: 'center',
+  swipe_cards: {
+    flex: 1,
+    borderColor: '#01ad01'
   }
 });
+
+
+// <View style={styles.treeviewer >
+//   <View>
+//     <Tree name={this.props.name} imageURL={this.props.imageURL} description={this.props.description} />
+//   </View>
+//   <View stlye= {styles.axe_water_container}>
+//     <Axe />
+//     <Water />
+//   </View>
+// </View>
