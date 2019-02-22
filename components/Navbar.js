1202938c9c951ab95b1b4e28ed8eea8271dd58ef
felixjtdb <felixjtdb@gmail.com>
2019-02-22 14:48:42 +0000

    Created Navbar Component and implemented Navigation within Main

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { StyleSheet, Button, View, Dimensions } from 'react-native'
+import { createStackNavigator, createAppContainer, StackActions, NavigationActions } from 'react-navigation';
 
-var navbarHeight = Dimensions.get('window').height / 8
-var navbarItemWidth = Dimensions.get('window').width / 3
+var navbarItemHeight = Dimensions.get('window').height / 10
+var navbarWidth = Dimensions.get('window').width / 5
+var navbarHeight = navbarItemHeight * 1.5
 
 class Navbar extends Component {
   render() {
     return (
       <View style={styles.navbar_container}>
-        <Text style={styles.navbar_item}> Upload Tree! </Text>
-        <Text style={styles.navbar_item}> TreeViewer! </Text>
-        <Text style={styles.navbar_item}> Forest! </Text>
+        <Button style={styles.navbar_item} title={'Trees!'} onPress={() => {
+          this.props.navigation.navigate('TreeViewer', {
+            navigation: this.props.navigation
+          })
+        }}/>
+        <Button style={styles.navbar_item} title={'Forest!'} onPress={() => {
+            this.props.navigation.navigate('Forest', {
+              navigation: this.props.navigation
+            })
+          }}/>
       </View>
     );
   }
@@ -20,16 +29,19 @@ export default Navbar;
 
 const styles = StyleSheet.create({
   navbar_container: {
-    backgroundColor: '#f782b9',
-    flexDirection: 'row',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: navbarWidth,
     height: navbarHeight,
-    alignItems: 'center',
-    alignContent: 'center'
+    zIndex: 1,
+    position: 'absolute',
+    top: 50,
+    backgroundColor: '#b0b0b0'
+
   },
   navbar_item: {
-    flex: 1,
-    justifyContent: 'center',
-    width: navbarItemWidth,
-    height: navbarHeight,
+    height: navbarItemHeight,
+    borderColor: '#6d2edc',
+    borderRadius: 40
   }
 });
