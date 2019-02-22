d39d9b8d1df37c8efd55c1451d2b5e3b78134c9f
felixjtdb <felixjtdb@gmail.com>
2019-02-22 15:46:57 +0000

    Added Images for Forest and Treeviewer, Removed Axe and Water images

--- components/Main.js
+++ components/Main.js
@@ -3,21 +3,6 @@ import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import Forest from './Forest'
 import TreeViewer from './TreeViewer'
 
-const testTrees =
-  [ { id : 1,
-      name: 'Oak',
-      imageURL: 'https://www.fast-growing-trees.com/images/P/Live-Oak-Tree-450w.jpg',
-      description: "I'm a big ol tree",
-      location: 123
-    },
-    { id : 2,
-      name: 'BrOak',
-      imageURL: "http://www.irishtreesociety.com/wp-content/uploads/irish-tree-society-feature.jpg",
-      description: "me too!",
-      location: 456
-    }
-  ]
-
 const Main = createSwitchNavigator({
   TreeViewer: {
     screen: TreeViewer
